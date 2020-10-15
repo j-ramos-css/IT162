488d04faee7f76aec857db73fe725e8962a6b4e5
Julio Ramos <64497770+j-ramos-css@users.noreply.github.com>
2020-10-14 21:55:19 -0700

    Updated website
    Created PHP files and updated the site

---
+++ IT162/includes/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Julio Ramos IT 162 Portal</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="robots" content="noindex,nofollow" />
+	<meta charset="utf-8">
+	<link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
+	<script src="https://use.fontawesome.com/6a71565c22.js"></script>
+	<link rel="stylesheet" href="css/nav.css" />
+	<link rel="stylesheet" href="css/portal.css" />
+	<link rel="stylesheet" href="css/form.css" />
+</head>
+
+<body>
+	<!-- START WRAPPER -->
+	<main class="wrapper">
+		<header>
+			<h1 id="header"><a href="index.php"><i class="fas fa-igloo"></i>Julio Ramos IT162 Portal</a></h1>
+			<nav>
+				<ul class="topnav" id="myTopnav">
+					<li><a href="index.php" class="selected">Welcome</a></li>
+					<li><a href="big/index.php">Big</a></li>
+					<li><a href="aia.php">AIA</a></li>
+					<li><a href="flowchart.php">Flowchart</a></li>
+					<li><a href="fp/index.php">Final Project</a></li>
+					<li><a href="contactme.php">Contact Julio</a></li>
+					<li class="icon"> <a href="javascript:void(0);" onclick="myFunction()">&#9776;</a> </li>
+
+				</ul>
+			</nav>
+		</header>
