1dd1dfab2c60e672c708aa5e6a023a74bccb9ecd
Julio Ramos <64497770+j-ramos-css@users.noreply.github.com>
2020-10-17 19:21:37 -0700

    Created portal-config.php

---
+++ contactme.php
@@ -0,0 +1,19 @@
+<?php include 'includes/header.php';?>
+<!-- Header ends here -->
+<!-- START LEFT COL -->
+<section>
+
+	<h2>PHP Template</h2>
+	<p>Use as a template for creating your other pages</p>
+
+</section>
+<!-- END LEFT COL -->
+
+<!-- START RIGHT COL -->
+<aside>
+	
+</aside>
+<!-- END RIGHT COL -->
+
+<!-- START Footer -->
+<?php include 'includes/footer.php';?>
---
+++ index.php
@@ -0,0 +1,26 @@
+<?php include 'includes/header.php';?>
+<!-- Header ends here -->
+<!-- START LEFT COL -->
+<section>
+
+	<h2 class="PageID"><?=$PageID?></h2>
+
+	<img src="images/desktop.jpg" class="desktop" alt="Desktop" />
+	<img src="images/phone.jpg" class="phone" alt="Phone" />
+	<p>Hi, my name is Julio and I'm an aspiring web designer/developer and have been working towards a degree for a year. Initially I was looking towards the graphic design world but decided to shhift gears.</p>
+	<p>Initially from New Hampshire, I moved to Portland when I was 18 which opened my eyes to the amazing design produced in the PNW.</p>
+	<p>My focus has been towards design and the user. I believe a website must not only function properly, but exceed a users expectations in terms of usability and design. My goal is to create these websites with a company that I can grow with.</p>
+</section>
+<!-- END LEFT COL -->
+
+<!-- START RIGHT COL -->
+<aside>
+	<h3>Right Column</h3>
+	<img src="images/tablet.jpg" class="tablet" alt="" />
+	<p>Lorem Ipsum place holder text goes here for now</p>
+	<p>Lorem Ipsum place holder text goes here for now</p>
+</aside>
+<!-- END RIGHT COL -->
+
+<!-- START Footer -->
+<?php include 'includes/footer.php';?>
