8820f7e0e2fb7561145cedf9ce3ae4d36d7ad921
Julio Ramos <64497770+j-ramos-css@users.noreply.github.com>
2020-10-17 20:10:49 -0700

    Contact form

--- contactme.php
+++ contactme.php
@@ -3,9 +3,10 @@
 <!-- START LEFT COL -->
 <section>
 
-	<h2>PHP Template</h2>
-	<p>Use as a template for creating your other pages</p>
+	<h2>Contact Julio</h2>
+	<p>Cheers!</p>
 
+	
 </section>
 <!-- END LEFT COL -->
 
@@ -14,6 +15,25 @@
 	
 </aside>
 <!-- END RIGHT COL -->
+
+<?php
+        /*
+         * Below are 2 different forms to be re-used       
+         * 
+         * Only use one at a time, comment out the other!       
+         *
+         */
+
+        include 'includes/contact_include.php'; #site keys & code here
+    
+        $toAddress = "julioramos1993@gmail.com";  //place your/your client's email address here
+        $toName = "Julio"; //place your client's name here
+        $website = "IT162";  //place NAME of your client's website
+
+        //echo loadContact('simple.php');#demonstrates a simple contact form
+        echo loadContact('multiple.php');#demonstrates multiple form elements
+
+	?>
 
 <!-- START Footer -->
 <?php include 'includes/footer.php';?>
